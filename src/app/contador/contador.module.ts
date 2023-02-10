e8373a1ae236c479b7cf5c5ba0031eb1aeee6dbd
Gabriel Salazar <gabriel.salazar@greensoft.com.ec>
2023-02-10 11:46:05 -0500

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,15 @@
+import { ContadorComponent } from './contador/contador.component';
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [
+        ContadorComponent,
+    ],
+    imports: [
+
+    ],
+    exports: [
+        ContadorComponent,
+    ]
+})
+export class contadorModule {}
